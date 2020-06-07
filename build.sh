f47f429f15cfc48b45505900601cb9acd2321951
Carlos Mendes <cmendesce@gmail.com>
2020-06-07 14:39:50 -0300

    Add 100k images

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in '20k' '200k' '400k' '600k' '800k' 'text'; do
+for i in '20k' '100k' '200k' '400k' '600k' '800k' 'text'; do
     docker build --build-arg BUILDTIME_FIDELITY=$i -t cmendes/znn:$i .
     docker push cmendes/znn:$i
 done
